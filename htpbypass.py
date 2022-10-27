2a234b3550a966c0de6f6e06a6a094b737187d77
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-10-27 18:40:57 +0530

    Update htpbypass.py

--- htpbypass.py
+++ htpbypass.py
@@ -5,36 +5,24 @@ from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 from requests import get
 
-url = "https://htpmovies.art/exit.php?url=M3hGellLam5SSmI0Q3FHU01sRFI1UWtha2YrOWpRWGMwcEtWU1F2NXkyd3ljeVFxM2ZubVlpczUxMXlHM3hqSg=="
+url = "https://htpmovies.lol/exit.php?url=M3hGellLam5SSmI0Q3FHU01sRFI1UWtha2YrOWpRWGMwcEtWU1F2NXkyd3ljeVFxM2ZubVlpczUxMXlHM3hqSg=="
 
 def htp(url):
-    yurl = url.replace("exit", "go")
-    download = get(yurl, stream=True, allow_redirects=False) 
-    xurl =download.headers["location"]
+    download = get(url, stream=True, allow_redirects=False) 
+    xurl =download.headers["location"]   
     client = cloudscraper.create_scraper(allow_brotli=False)
-    p = urlparse(xurl)
-    final_url = f'{p.scheme}://{p.netloc}/links/go'
-    res = client.head(xurl)
-    header_loc = res.headers['location']
     param = xurl.split("/")[-1]
-    req_url = f'{p.scheme}://{p.netloc}/{param}'
-    p = urlparse(header_loc)
-    ref_url = f'{p.scheme}://{p.netloc}/'
-    h = { 'referer': ref_url }
-    res = client.get(req_url, headers=h, allow_redirects=False)
-
-    bs4 = BeautifulSoup(res.content, 'html.parser')
-    inputs = bs4.find_all('input')
+    DOMAIN = "https://go.kinemaster.cc"
+    final_url = f"{DOMAIN}/{param}"
+    resp = client.get(final_url)
+    soup = BeautifulSoup(resp.content, "html.parser")    
+    try: inputs = soup.find(id="go-link").find_all(name="input")
+    except: return "Incorrect Link"
     data = { input.get('name'): input.get('value') for input in inputs }
-
-    h = {
-        'referer': ref_url,
-        'x-requested-with': 'XMLHttpRequest',
-    }
+    h = { "x-requested-with": "XMLHttpRequest" }
     time.sleep(10)
-    res = client.post(final_url, headers=h, data=data)
+    r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
     try:
-        return res.json()['url'].replace('\/','/')
-    except: return 'Something went wrong :('
-
+        return r.json()['url']
+    except: return "Something went wrong :("
 print(htp(url))

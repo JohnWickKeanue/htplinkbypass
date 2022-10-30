70c8c5303d30f872388100cfa5ddb768faf52f38
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-10-30 14:28:19 +0530

    Update htpbypass.py

--- htpbypass.py
+++ htpbypass.py
@@ -1,17 +1,18 @@
-
 import time
 import cloudscraper
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-from requests import get
+from requests import get, head
 
 url = "https://htpmovies.lol/exit.php?url=M3hGellLam5SSmI0Q3FHU01sRFI1UWtha2YrOWpRWGMwcEtWU1F2NXkyd3ljeVFxM2ZubVlpczUxMXlHM3hqSg=="
 
 def htp(url):
-    download = get(url, stream=True, allow_redirects=False) 
-    xurl =download.headers["location"]   
+    if url.startswith("https://htpmovies.lol/"):
+        r = head(url, allow_redirects=True)
+        url = r.url
     client = cloudscraper.create_scraper(allow_brotli=False)
-    param = xurl.split("/")[-1]
+    j = url.split('?token=')[-1]
+    param = j.replace('&m=1','')
     DOMAIN = "https://go.kinemaster.cc"
     final_url = f"{DOMAIN}/{param}"
     resp = client.get(final_url)
